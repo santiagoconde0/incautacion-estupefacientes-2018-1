55a5971edc9e643005601a775cf812b5c93780b4
santiago <santiagoconde03@hotmail.com>
2018-09-26 14:54:11 -0500

    update files

--- js/main.js
+++ js/main.js
@@ -1,4 +1,220 @@
 const url = "https://raw.githubusercontent.com/santiagoconde0/incautacion-estupefacientes-2018-1/master/data/data.csv";
+d3.csv(url).then(data => { // Get Data
+
+  var margin = {
+      top: 50,
+      right: 90,
+      bottom: 50,
+      left: 113
+    },
+    width = 1100,
+    height = 1000,
+    radius = Math.min(width, height) / 2,
+    iwidth = width - margin.left - margin.right,
+    iheight = height - margin.top - margin.bottom;
+
+  // console.log("iwidth: " + iwidth + "    " + "iheight: " + iheight);x
+
+  const listMunicipio = {
+    dat: d3
+      .nest().key(d => {
+        return d.MUNICIPIO;
+      })
+      .rollup(d => {
+        return d3.sum(d, d => +d.CANTIDAD);
+      })
+      .entries(data)
+  };
+
+  var maxCant = d3.max(listMunicipio.dat, d => +d.value);
+  var minCant = d3.min(listMunicipio.dat, d => +d.value);
+
+  var svg = d3 // create the SVG
+    .select("#target4")
+    .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+  var numDep = document.querySelector("input[id='slNumBars']").value;
+
+  const filteredData = listMunicipio.dat //order
+    .sort((a, b) => d3.descending(a.value, b.value))
+    .slice(0, numDep);
+
+  console.log(filteredData);
+
+  const filteredData2 = filteredData.filter(function(d) { // delet the undefine datas
+    if (d.value > 0) {
+      return true;
+    }
+  });
+
+  const departamentos = d3.set(data.map(d => d.DEPARTAMENTO)).values();
+
+  var x2 = d3.scaleLinear() // y axis scale
+    .domain([minCant, maxCant])
+    .range([minCant, iheight]);
+
+  // console.log(minCant + "   " +maxCant);
+
+  const y2 = d3.scaleBand() // x axis scale
+    .padding(0.1)
+    .domain(filteredData2.map(d => d.key))
+    .range([0, iwidth]);
+
+  svg.append("g") // Y axis
+    .call(d3.axisLeft(y2));
+
+  svg.append("g") // X axis
+    .attr("transform", `translate(0,${iheight})`)
+    .call(d3.axisBottom(x2));
+
+
+  console.log("listamunicipios: ");
+
+  console.log(listMunicipio.dat.map(d => +(d.value)));
+
+  svg.selectAll("rect") // insrt bars
+    .data(listMunicipio.dat)
+    .enter()
+    .append("rect")
+    .attr("class", "bar")
+    .style("visibility", "visible")
+    .attr("x", 3)
+    .attr("y", function(d) {
+      return y2(d.key);
+    })
+    .attr("width", d => +x2(d.value))
+    .attr("height", y2.bandwidth())
+    .attr("fill", "#77dd77");
+
+});
+
+// --------------------------------------------------------------------
+// --------------------------- On change grafico 1 ------------------
+// --------------------------------------------------------------------
+
+d3.csv(url).then(data => { // Get Data
+
+  var margin = {
+      top: 50,
+      right: 90,
+      bottom: 50,
+      left: 113
+    },
+    width = 1100,
+    height = 1000,
+    radius = Math.min(width, height) / 2,
+    iwidth = width - margin.left - margin.right,
+    iheight = height - margin.top - margin.bottom;
+
+  // console.log("iwidth: " + iwidth + "    " + "iheight: " + iheight);x
+  const munLength = d3.set(data.map(d => d.MUNICIPIO)).values();
+
+  d3.select("#slNumBars")
+    .on('change', onchange2);
+
+  console.log("Length", munLength.length);
+
+  function onchange2() { // change the values
+
+    console.log("Length:  ", munLength.length);
+    d3.select("#target4").remove(); // clear the bars
+
+    d3.select('#targetD')
+      .append('div')
+      .attr("id", "target4");
+
+    var numDep = document.querySelector("input[id='slNumBars']").value; //get the slide value
+
+    console.log("Change");
+    console.log("Value: " + numDep);
+    // }; // Close here
+
+    const listMunicipio = {
+      dat: d3
+        .nest().key(d => {
+          return d.MUNICIPIO;
+        })
+        .rollup(d => {
+          return d3.sum(d, d => +d.CANTIDAD);
+        })
+        .entries(data)
+    };
+
+    var maxCant = d3.max(listMunicipio.dat, d => +d.value);
+    var minCant = d3.min(listMunicipio.dat, d => +d.value);
+
+    var svg = d3 // create the SVG
+      .select("#target4")
+      .append("svg")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
+      .append("g")
+      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+
+
+    const filteredData = listMunicipio.dat //order
+      .sort((a, b) => d3.descending(a.value, b.value))
+      .slice(0, numDep);;
+
+    console.log(filteredData);
+
+    const filteredData2 = filteredData.filter(function(d) { // delet the undefine datas
+      if (d.value > 0) {
+        return true;
+      }
+    });
+
+    const departamentos = d3.set(data.map(d => d.DEPARTAMENTO)).values();
+
+    var x2 = d3.scaleLinear() // y axis scale
+      .domain([minCant, maxCant])
+      .range([minCant, iheight]);
+
+
+    // console.log(minCant + "   " +maxCant);
+
+    const y2 = d3.scaleBand() // x axis scale
+      .padding(0.1)
+      .domain(filteredData2.map(d => d.key))
+      .range([0, iwidth]);
+
+    svg.append("g") // Y axis
+      .call(d3.axisLeft(y2));
+
+    svg.append("g") // X axis
+      .attr("transform", `translate(0,${iheight})`)
+      .call(d3.axisBottom(x2));
+
+
+    console.log("listamunicipios: ");
+
+    console.log(listMunicipio.dat.map(d => +(d.value)));
+
+    svg.selectAll("rect") // insrt bars
+      .data(listMunicipio.dat)
+      .enter()
+      .append("rect")
+      .attr("class", "bar")
+      .style("visibility", "visible")
+      .attr("x", 3)
+      .attr("y", function(d) {
+        return y2(d.key);
+      })
+      .attr("width", d => +x2(d.value))
+      .attr("height", y2.bandwidth())
+      .attr("fill", "#77dd77");
+  };
+
+});
+//
+// -----------------------------------------------------------------------------------------
+// Segunda Grafica
+// -----------------------------------------------------------------------------------------
 
 d3.csv(url).then(data => { // Get Data
 
@@ -104,6 +320,8 @@ d3.csv(url).then(data => { // Get Data
 });
 
 
+// --------- On change viz 2 ---------------------
+
 d3.csv(url).then(data => { // Get Data
 
   d3.select("#target")
@@ -115,7 +333,7 @@ d3.csv(url).then(data => { // Get Data
 
   d3.select("#target")
     .append("h6")
-    .text("Seleccione la cantidad de municipios que desea visualizar");
+    .text("Seleccione el departamento que desea visualizar");
 
 
   const departamentos = d3.set(data.map(d => d.DEPARTAMENTO)).values();
@@ -132,14 +350,15 @@ d3.csv(url).then(data => { // Get Data
     .append("option")
     .text(d => d);
 
+    d3.select("#selector")
+      .property("value" , "");
+
   function onchange() {
-    d3.select("svg").remove();
     d3.select("#target2").remove();
 
     d3.select('#target')
       .append('div')
       .attr("id", "target2")
-
     update();
   };
 
@@ -257,7 +476,7 @@ d3.csv(url).then(data => { // Get Data
 
 d3.select("#target3")
   .append("h3")
-  .text("Insight No.1");
+  .text("Insight No.2");
 
 d3.select("#target3")
   .append("h5")
@@ -267,111 +486,3 @@ d3.select("#target3")
   .append("br");
 d3.select("#target3")
   .append("br");
-
-d3.select("#target4")
-  .append("h3")
-  .text("¿Cuál es el municipio con más incautaciones de estupesfacientes?");
-
-//
-// -----------------------------------------------------------------------------------------
-// Segunda Grafica
-// -----------------------------------------------------------------------------------------
-
-
-d3.csv(url).then(data => { // Get Data
-
-  var margin = {
-      top: 50,
-      right: 90,
-      bottom: 50,
-      left: 115
-    },
-    width = 1100,
-    height = 1000,
-    radius = Math.min(width, height) / 2,
-    iwidth = width - margin.left - margin.right,
-    iheight = height - margin.top - margin.bottom;
-
-  // console.log("iwidth: " + iwidth + "    " + "iheight: " + iheight);x
-
-
-  const listMunicipio = {
-    dat: d3
-      .nest().key(d => {
-        return d.MUNICIPIO;
-      })
-      .rollup(d => {
-        return d3.sum(d, d => +d.CANTIDAD);
-      })
-      .entries(data)
-  };
-
-  var maxCant = d3.max(listMunicipio.dat, d => +d.value);
-  var minCant = d3.min(listMunicipio.dat, d => +d.value);
-
-  var svg = d3 // create the SVG
-    .select("#target4")
-    .append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-    .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-
-
-  const filteredData = listMunicipio.dat //order
-    .sort((a, b) => d3.descending(a.value, b.value))
-    .slice(0, 25);;
-
-  console.log(filteredData);
-
-  const filteredData2 = filteredData.filter(function(d) { // delet the undefine datas
-    if (d.value > 0) {
-      return true;
-    }
-  });
-
-  const departamentos = d3.set(data.map(d => d.DEPARTAMENTO)).values();
-
-  var x2 = d3.scaleLinear() // y axis scale
-    .domain([minCant, maxCant])
-    .range([minCant, iheight]);
-
-
-  // console.log(minCant + "   " +maxCant);
-
-  const y2 = d3.scaleBand() // x axis scale
-    .padding(0.1)
-    .domain(filteredData2.map(d => d.key))
-    .range([0, iwidth]);
-
-  svg.append("g") // Y axis
-    .call(d3.axisLeft(y2));
-
-  svg.append("g") // X axis
-    .attr("transform", `translate(0,${iheight})`)
-    .call(d3.axisBottom(x2));
-
-
-  console.log("listamunicipios: ");
-
-  console.log(listMunicipio.dat.map(d => +(d.value)));
-
-  svg.selectAll("rect") // insrt bars
-    .data(listMunicipio.dat)
-    .enter()
-    .append("rect")
-    .attr("class", "bar")
-    .style("visibility", "visible")
-    .attr("x", 3)
-    .attr("y", function(d) {
-      return y2(d.key);
-    })
-    .attr("width", d => +x2(d.value))
-    .attr("height", y2.bandwidth())
-    .attr("fill", "#77dd77");
-
-
-
-
-});
